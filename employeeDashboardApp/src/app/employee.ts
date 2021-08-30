bc01c9119fab0062a096bd408a33555a7c698756
JulianaMedinaF <juanapiola@hotmail.com>
2021-08-30 00:18:02 -0400

    Starting dashboard employees in algular

---
+++ employeeDashboardApp/src/app/employee.ts
@@ -0,0 +1,12 @@
+
+export interface Employee {
+    id: number;
+    first_name: string;
+    last_name: string;
+    address: string;
+    telephone_number: string;
+    hiring_date: Date;
+    type_employee:string;
+    date_birth: Date;
+
+}
