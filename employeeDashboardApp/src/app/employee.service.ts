82c4ae8cab17f4bee114d87d5ba038b21f91dde9
JulianaMedinaF <juanapiola@hotmail.com>
2021-11-08 21:11:08 -0500

    Adding better interface and adding buttons for edit, add and delete (using boostrap)

--- employeeDashboardApp/src/app/employee.service.ts
+++ employeeDashboardApp/src/app/employee.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: null
 })
 
 export class EmployeeService {
